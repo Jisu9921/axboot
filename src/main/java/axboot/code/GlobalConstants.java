b4f6de6232e5e1f98bb182830e54d6bd018cf969
CHOI jisu <h41469921@gmail.com>
2018-02-05 11:32:48 +0900

    axboot 초기 셋업

---
+++ src/main/java/axboot/code/GlobalConstants.java
@@ -0,0 +1,14 @@
+package axboot.code;
+
+public class GlobalConstants {
+
+    public static final String DOMAIN_PACKAGE = "axboot.domain";
+
+    public static final String LAST_NAVIGATED_PAGE = "a_x_b_l_n_p";
+
+    public static final String ADMIN_AUTH_TOKEN_KEY = "e221c1f4-b112-4256-af65-711fe45efbc0";
+
+    public static final String LANGUAGE_COOKIE_KEY = "a_x_lang";
+
+    public static final String LANGUAGE_PARAMETER_KEY = "language";
+}
